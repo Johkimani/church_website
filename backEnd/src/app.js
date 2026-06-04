0da277fa4666a6cffebd3b693888905683204575
Anedo_ras <johnkennedyonyango12>
2026-06-04 17:42:03 +0300

    feat: enable CORS specifically for the mpesa callback route

--- backEnd/src/app.js
+++ backEnd/src/app.js
@@ -58,6 +58,8 @@ const io = new Server(httpServer, {
 initializeSocketIO(io)
 setSocketInstance(io);
 
+app.use("/api/v1/authentication/mpesa/callback", cors());
+
 // this is the best way to to get the actual ip adress of a device even if the server is behind a proxy
 //rather than getting the proxy ip adress , usefull in fare shairing of resorces
 app.use(requestIp.mw());
